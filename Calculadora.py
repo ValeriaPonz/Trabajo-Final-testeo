29693f06c941e720b598797f55dc0c92ece4d08d
liv04-20 <lz.duran@outlook.es>
2026-02-13 13:00:48 +0100

    cambios

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,7 @@
 # Carolina, Livia y Valeria
+
 def sumar(a, b):
-    return a - b
+    return a + b
 
 
 def restar(a, b):
--- test_funcion.py
+++ test_funcion.py
@@ -1,7 +1,7 @@
 # Funciones
 import unittest
-from Calculadora import sumar
-from Calculadora import restar
+from Calculadora import sumar, restar
+
 class sTestCalculadora(unittest.TestCase):
     def test_sumar_positivos(self):
         self.assertEqual(sumar(2, 3), 5)
@@ -12,9 +12,12 @@ class sTestCalculadora(unittest.TestCase):
         self.assertEqual(sumar(-10, -15), -25)
         
     def test_restar_positivos(self):
-        self.assertEqual(restar(5, -2), 3)
-        self.assertEqual(restar(15, -10), 5)
+        self.assertEqual(restar(5, -2), 7)
+        self.assertEqual(restar(15, -10), 25)
     
     def test_restar_negativos(self):
-        self.assertEqual(restar(-5, 2), -3)
-        self.assertEqual(restar(-15, 10), -5)
+        self.assertEqual(restar(-5, 2), -7)
+        self.assertEqual(restar(-15, 10), -25)
+
+if __name__ == '__main__':
+    unittest.main()
