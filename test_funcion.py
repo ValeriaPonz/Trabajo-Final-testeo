50b57027b076b4e496edd009c2de4299c77d8f43
liv04-20 <lz.duran@outlook.es>
2026-02-13 13:54:26 +0100

    cambios

--- test_funcion.py
+++ test_funcion.py
@@ -1,6 +1,6 @@
 # Funciones
 import unittest
-from Calculadora import sumar, restar, es_par, es_mayor_edad
+from Calculadora import sumar, restar, multiplicar, dividir, es_par, es_mayor_edad
 
 class sTestCalculadora(unittest.TestCase):
     def test_sumar_positivos(self):
