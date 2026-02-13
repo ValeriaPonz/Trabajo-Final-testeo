f2f4a20f595944d7b6bd04cdbe94b453bf16d999
Valeria <valeria.loslipo@gmail.com>
2026-02-13 12:45:38 +0100

    cambios

--- test_funcion.py
+++ test_funcion.py
@@ -1,7 +1,8 @@
 # Funciones
 import unittest
 from Calculadora import sumar
-class TestCalculadora(unittest.TestCase):
+from Calculadora import restar
+class sTestCalculadora(unittest.TestCase):
     def test_sumar_positivos(self):
         self.assertEqual(sumar(2, 3), 5)
         self.assertEqual(sumar(10, 15), 25)
@@ -9,11 +10,11 @@ class TestCalculadora(unittest.TestCase):
     def test_sumar_negativos(self):
         self.assertEqual(sumar(-2, -3), -5)
         self.assertEqual(sumar(-10, -15), -25)
-
+        
     def test_restar_positivos(self):
-        self.assertEqual(sumar(5, -2), 3)
-        self.assertEqual(sumar(15, -10), 5)
+        self.assertEqual(restar(5, -2), 3)
+        self.assertEqual(restar(15, -10), 5)
     
     def test_restar_negativos(self):
-        self.assertEqual(sumar(-5, 2), -3)
-        self.assertEqual(sumar(-15, 10), -5)
+        self.assertEqual(restar(-5, 2), -3)
+        self.assertEqual(restar(-15, 10), -5)
