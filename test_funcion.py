a47f84b96d1f0945091343712e0e27fc1b89fb2b
liv04-20 <lz.duran@outlook.es>
2026-02-13 13:18:14 +0100

    cambios

--- test_funcion.py
+++ test_funcion.py
@@ -1,6 +1,6 @@
 # Funciones
 import unittest
-from Calculadora import sumar, restar
+from Calculadora import sumar, restar, es_par, es_mayor_edad
 
 class sTestCalculadora(unittest.TestCase):
     def test_sumar_positivos(self):
@@ -18,6 +18,22 @@ class sTestCalculadora(unittest.TestCase):
     def test_restar_negativos(self):
         self.assertEqual(restar(-5, 2), -7)
         self.assertEqual(restar(-15, 10), -25)
+
+    def test_es_par_positivos(self):
+        self.assertTrue(es_par(2))
+        self.assertTrue(es_par(4))
+
+    def test_es_par_negativos(self):
+        self.assertTrue(es_par(-2))
+        self.assertTrue(es_par(-8))
+    
+    def test_es_mayor_edad_positivos(self):
+        self.assertTrue(es_mayor_edad(20))
+        self.assertTrue(es_mayor_edad(19))
+    
+    def test_es_mayor_edad_negativos(self):
+        self.assertFalse(es_mayor_edad(10))
+        self.assertFalse(es_mayor_edad(15))
 
 if __name__ == '__main__':
     unittest.main()
