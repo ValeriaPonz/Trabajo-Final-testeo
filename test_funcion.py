701d83e538f0ceb1efb34b5f954f97a000d17733
Carolmp92 <c.medrano92@hotmail.com>
2026-02-20 09:40:59 +0100

    .

--- test_funcion.py
+++ test_funcion.py
@@ -6,7 +6,7 @@ class sTestCalculadora(unittest.TestCase):
     def test_sumar_positivos(self):
         self.assertEqual(sumar(2, 3), 5)
         self.assertEqual(sumar(10, 15), 25)
-
+ 
     def test_sumar_negativos(self):
         self.assertEqual(sumar(-2, -3), -5)
         self.assertEqual(sumar(-10, -15), -25)
@@ -22,11 +22,11 @@ class sTestCalculadora(unittest.TestCase):
         self.assertEqual(restar(-5, 2), -7)
         self.assertEqual(restar(-15, 10), -25)
 
-    def test_multipicacion_basica(self):
+    def test_multipicar_basica(self):
         self.assertEqual (multiplicar(2,3),6)
         self.assertEqual (multiplicar (-2,-3),6)
 
-    def tet_division_basica (self):
+    def tet_dividir_basica (self):
         self.assertEqual (dividir(8,2),4)
         self.assertEqual (dividir(-8, -2),4)
 
